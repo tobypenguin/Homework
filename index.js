2cb611b6e2b99d3d45b672cc61cc7c76bb11d26c
tobypenguin <pantsvszomebie@gmail.com>
2022-11-19 21:03:24 +0700

    Finish get_channelId and check_sub

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 let channel_id = [];
 let data = [];
+let dataCH = [];
 let result = [];
 let token = [];
 let totalResults = [];
@@ -8,18 +9,18 @@ let totalPages = [];
 let i = 0;
 
 let params = {
-  key: "AIzaSyBmXhsqA45sjMjcpii3d_YXJ7JcOZ6q1ko",
+  key: "AIzaSyCuvdW20d-RmXR1frQViK4-4UVkG435bdY",
   part: "snippet",
   chart: "mostPopular",
   regionCode: "TH",
   maxResults: 50,
-  pageToken: ""
+  pageToken: "",
 }
 
-
-
 async function get_channelId() {
-  await axios.get('https://www.googleapis.com/youtube/v3/videos', { params }).then( res => {
+  console.log("start getting channel id");
+  console.log("************************");
+  await axios.get('https://www.googleapis.com/youtube/v3/videos', { params }).then(res => {
     data = res.data.items
     // console.log(data);
     data.forEach(element => {
@@ -33,12 +34,12 @@ async function get_channelId() {
   totalResults = result.pageInfo.totalResults
   totalPages = Math.ceil(totalResults / 50);
 
-  console.log(totalResults);
-  console.log(totalPages);
+  /**console.log(totalResults);
+  console.log(totalPages);*/
 
   token = result.nextPageToken
   while (i < totalPages) {
-    console.log(token);
+    // console.log(token);
     params["pageToken"] = token
     await axios.get('https://www.googleapis.com/youtube/v3/videos', { params }).then(res => {
       token = res.data.nextPageToken
@@ -51,7 +52,40 @@ async function get_channelId() {
     i++;
   }
   console.log(channel_id);
+  console.log("get channel id end");
+  console.log("************************");
 }
 
+async function check_sub() {
+  console.log("start checking sub");
+  console.log("************************");
 
-get_channelId();
+  // forEach id and then get api 
+
+  channel_id.forEach(async element => {
+    await axios.get('https://www.googleapis.com/youtube/v3/channels', {
+      params: {
+        key: "AIzaSyCuvdW20d-RmXR1frQViK4-4UVkG435bdY",
+        part: "snippet,statistics",
+        id: element
+      }
+    }).then(res => {
+      dataCH.push(
+        {
+          Channel_id: element,
+          Channel_name: res.data.items[0].snippet.title,
+          Sub_count: !res.data.items[0].statistics.hiddenSubscriberCount ? res.data.items[0].statistics.subscriberCount : 0
+        })
+    });
+    console.log(dataCH);
+  })
+  
+}
+
+function Steps() {
+  Promise.resolve(get_channelId()).then(x => {
+    return check_sub();
+  })
+}
+
+Steps();
